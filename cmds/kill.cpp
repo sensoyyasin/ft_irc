c45817888317e7e95e00b825560db7e5229464be
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2023-05-14 01:30:06 +0300

    kill finished

--- cmds/kill.cpp
+++ cmds/kill.cpp
@@ -4,6 +4,7 @@
 
 void Server::kill(std::string buffer, int fd)
 {
+	//kill name reason
 	std::vector<std::string> my_vec;
 	unsigned int i = 0;
 
@@ -17,7 +18,7 @@ void Server::kill(std::string buffer, int fd)
 		my_vec.push_back(command);
 	}
 
-	if (my_vec.size() != 1){
+	if (my_vec.size() != 2){
 		std::cerr << "\033[1;91mError: Kill args\033[0m" << std::endl;
 		return;
 	}
@@ -27,7 +28,7 @@ void Server::kill(std::string buffer, int fd)
 		if (this->clients_[i].getNickName() == my_vec[0]) {
 			flag = true;
 			int target_fd = this->clients_[i].getFd();
-			std::string msg = ":" + this->client_ret(fd)->getNickName() + " KILL " + my_vec[0] + " :Killed by " + this->client_ret(fd)->getNickName() + "\r\n";
+			std::string msg = ":" + this->client_ret(fd)->getNickName() + " KILL " + my_vec[0] + " :Killed by " + this->client_ret(fd)->getNickName() + " " + my_vec[1] + "\r\n";
 			send(target_fd, msg.c_str(), msg.size(), 0);
 			close(target_fd);
 			this->clients_.erase(this->clients_.begin() + i);
