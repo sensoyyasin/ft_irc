83edfb079db1c2ea49be426f864cc71ba45b6472
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2023-05-13 10:56:51 +0300

    quit function solved

--- cmds/quit.cpp
+++ cmds/quit.cpp
@@ -6,14 +6,15 @@ void Server::quit(std::string str, int fd)
 {
 	if (this->flag == 0)
 	{
-		std::cout <<"\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m ";
+		std::cout << "\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m ";
 		exit(1);
 	}
+
 	if (this->client_ret(fd))
 	{
-		std::cout <<"\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m ";
+		std::cout << "\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m ";
 
-		//Get the message from the command string
+		// Quit message
 		unsigned int i = 0;
 		std::string command = "";
 		while (i < str.size() && (str[i] != '\r' && str[i] != '\n'))
@@ -21,30 +22,31 @@ void Server::quit(std::string str, int fd)
 
 		std::cout << command << std::endl;
 
-		//Find the index of the client to remove
+		// Get index of the client to remove
 		unsigned int index = 0;
 		for (; index < this->clients_.size(); ++index)
 		{
 			if (this->clients_[index].getFd() == fd)
 				break;
 		}
+
 		// Set the admin to the previous client if the current admin quits
-		if (this->clients_[index].getFd() == this->channels_[index].getchannelAdminFd())
+		int prevAdminFd = this->channels_[index].getchannelAdminFd();
+		if (fd == prevAdminFd)
 		{
-			if (index == 0 && this->clients_.size() > 1)
-				this->channels_[index].setchannelAdminFd(this->clients_[1].getFd());
-			else if (index == 1 && this->clients_.size() > 2)
-				this->channels_[index].setchannelAdminFd(this->clients_[2].getFd());
-			else if (index > 1)
-				this->channels_[index].setchannelAdminFd(this->clients_[index - 1].getFd());
-			else
-			{
-				// No other clients left in the channel
-				this->channels_[index].setchannelAdminFd(42);
+			// Loop back to the last client if current admin is the first one
+			int newAdminIndex;
+			int newAdminFd;
+			if (index == 0) {
+				newAdminIndex = this->clients_.size() - 1;
+			} else {
+				newAdminIndex = index - 1;
 			}
+			newAdminFd = this->clients_[newAdminIndex].getFd();
+			this->channels_[index].setchannelAdminFd(newAdminFd);
+
+			std::cout << "\033[1;92mAdmin changed to\033[0m " << this->client_ret(newAdminFd)->getNickName() << std::endl;
 		}
-		//we have to check
-		std::cout << "\033[1;92mLastAdmin_fd : \033[0m" << this->channels_[index].getchannelAdminFd() << std::endl;
 
 		// Remove the client from the vector and close the connection
 		this->clients_.erase(this->clients_.begin() + index);
@@ -52,11 +54,12 @@ void Server::quit(std::string str, int fd)
 
 		std::cout << "Current number of clients: " << this->clients_.size() << std::endl;
 
+		// If the channel has no clients left, close it
 		if (this->clients_.empty())
 		{
 			std::cout << "No clients left, closing the channel." << std::endl;
-			this->channels_[index].setchannelAdminFd(-1);
+			this->channels_.erase(this->channels_.begin() + index);
 		}
 	}
-	(void)str;
+	(void)str; // unused parameter warning
 }
