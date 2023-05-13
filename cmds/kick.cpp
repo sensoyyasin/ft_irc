3a867055e2d1b0cfea24c4a9485a0ee9d9f8dbcd
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2023-05-13 23:50:24 +0300

    Kick finished

--- cmds/kick.cpp
+++ cmds/kick.cpp
@@ -5,23 +5,27 @@
 void Server::kick(std::string buffer, int fd)
 {
 	std::vector<std::string> my_vec;
-	std::string command = "";
 	unsigned int i = 0;
 
 	while (buffer.size() > i)
 	{
 		std::string command = "";
 		while (i < buffer.size() && (buffer[i] != ' ' && buffer[i] != '\r' && buffer[i] != '\n'))
-			command += buffer[i++]; //first ->command
+			command += buffer[i++];
 		while (i < buffer.size() && (buffer[i] == ' ' || buffer[i] == '\r' || buffer[i] == '\n'))
 			i++;
 		my_vec.push_back(command);
 	}
-	// gelen arg kanllarda var mı kontrolü.
+
 	this->channel_ok = 0;
 
+	// Kanal ve kullanıcı kontrolü yapılacak
+	if (my_vec.size() < 3) {
+		std::cerr << "\033[1;91mHatalı komut formatı. Örnek kullanım: /kick #kanal kullanıcı_adı\033[0m" << std::endl;
+		return;
+	}
+
 	// Get index of the client to remove (we found index for inside the channels.)
-	//düzeltilecek
 	unsigned int index = 0;
 	while (this->channels_.size() > index)
 	{
@@ -33,14 +37,15 @@ void Server::kick(std::string buffer, int fd)
 		index++;
 	}
 
-	if (this->channel_ok == 0)
+	if (!this->channel_ok)
 	{
-		std::cerr << "\033[1;91mError: Böyle bir kanal yok veya oluşmamiş..\033[0m" << std::endl;
+		std::cerr << "\033[1;91mYou are not an admin or you are trying to kick someone from a different channel.\033[0m" << std::endl;
 		return;
 	}
 
-	my_vec[2] = my_vec[2].substr(1, my_vec[2].size() - 1); //-> parsing #berk to berk.
-	int kick_fd = 42;
+	my_vec[2] = my_vec[2].substr(1); //-> Remove the # character
+
+	int kick_fd = -1;
 	for (unsigned int s = 0; s < this->clients_.size(); s++)
 	{
 		if (this->clients_[s].getNickName() == my_vec[2])
@@ -48,6 +53,20 @@ void Server::kick(std::string buffer, int fd)
 			kick_fd = this->clients_[s].getFd();
 			break;
 		}
+	}
+
+	// Check if user exists
+	if (kick_fd == -1)
+	{
+		std::cerr << "\033[1;91mUser not found.\033[0m" << std::endl;
+		return;
+	}
+
+	// Check if the admin is trying to kick himself
+	if (kick_fd == fd)
+	{
+		std::cerr << "\033[1;91mYou cannot kick yourself.\033[0m" << std::endl;
+		return;
 	}
 
 	//kanal varsa admin diye mi bak.
@@ -60,18 +79,13 @@ void Server::kick(std::string buffer, int fd)
 			{
 				if (this->channels_[index]._clientsFd[m] == kick_fd)
 				{
-					if (m == 0)
-					{
-						std::cerr << "Admin kendini atamaz" << std::endl;
-						return;
-					}
 					this->channels_[index]._clientsFd.erase(this->channels_[index]._clientsFd.begin() + m);
 					this->channels_[index].setClientCount(this->channels_[index].getchannelUserCount() - 1);
 				}
 				m++;
 			}
 	}
-	std::string msg = ":" + this->client_ret(fd)->getNickName() + " KICK " + my_vec[1] + " " + my_vec[2] + " :Speaking English \r\n";
+	std::string msg = ":" + this->client_ret(fd)->getNickName() + " KICK " + my_vec[1] + " " + my_vec[2] + " :Speaking English\r\n";
 	send(fd, msg.c_str(), msg.size(), 0);
 	buffer.clear();
 }
