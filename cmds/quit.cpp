a6940457fe791e2d1ebafb372463c189bb656ed1
Yasin Sensoy <ysensoy@k2m05s06.42kocaeli.com.tr>
2023-05-17 11:49:15 +0300

    quit problem solved

--- cmds/quit.cpp
+++ cmds/quit.cpp
@@ -6,8 +6,7 @@ void Server::quit(std::string str, int fd)
 {
 	if (this->flag == 0)
 	{
-		//There is no join
-		std::cerr << "\033[1;91mError: " << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m" << std::endl;
+		std::cout << "\033[1;91mError: " << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m ";
 		exit(1);
 	}
 
@@ -38,9 +37,17 @@ void Server::quit(std::string str, int fd)
 		i++;
 	}
 
-	if (this->client_ret(fd) && flag == true)
+	if (this->client_ret(fd))
 	{
-		std::cout << "\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m" << std::endl;
+		std::cout << "\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m ";
+
+		// Quit message
+		unsigned int i = 0;
+		std::string command = "";
+		while (i < str.size() && (str[i] != '\r' && str[i] != '\n'))
+			command += str[i++];
+
+		std::cout << command << std::endl;
 
 		// Get index of the client to remove
 		unsigned int index = 0;
@@ -50,6 +57,7 @@ void Server::quit(std::string str, int fd)
 
 		// Set the admin to the previous client if the current admin quits
 		unsigned int x = 0;
+
 		while (this->channels_.size() > x)
 		{
 			unsigned int k = 0;
@@ -60,20 +68,19 @@ void Server::quit(std::string str, int fd)
 					if (this->channels_[x].getchannelUserCount() == 1) // kanalı sil
 					{
 						this->channels_[x].setchannelAdminFd(-1);
-						this->channels_[x]._clientsFd.erase(this->channels_[x]._clientsFd.begin() + k); // Kanaldan kullanıcıyı sil
 						this->channels_[x]._clientsFd.pop_back();
+
+						this->channels_[x]._clientsFd.erase(this->channels_[x]._clientsFd.begin() + k); // Kanaldan kullanıcıyı sil
 						this->channels_.erase(this->channels_.begin() + x);
-						std::cout << "Burada mi3" << std::endl;
-						break;
 					}
-					else // change to client admin
+					else // Kanalın adminliği değişiyor
 					{
 						this->channels_[x].setchannelAdminFd(this->channels_[x]._clientsFd[1]);
 						this->channels_[x]._clientsFd.erase(this->channels_[x]._clientsFd.begin() + k);  // Kanaldan kullanıcıyı sil
 					}
 					this->channels_[x].setClientCount(this->channels_[x].getchannelUserCount() - 1);
 				}
-				else // user set
+				else //user
 				{
 					this->channels_[x]._clientsFd.erase(this->channels_[x]._clientsFd.begin() + k); // Kanaldan kullanıcıyı sil
 					this->channels_[x].setClientCount(this->channels_[x].getchannelUserCount() - 1);
@@ -87,11 +94,7 @@ void Server::quit(std::string str, int fd)
 		send(fd, msg.c_str(), msg.size(), 0);
 		// Remove the client from the vector and close the connection
 		this->clients_.erase(this->clients_.begin() + index);
-		//close(fd);
+		close(fd);
 	}
-	else
-	{
-		std::cerr << "\033[1;91mError...\033[0m" << std::endl;
-		return;
-	}
+	(void)str; // unused parameter warning
 }
